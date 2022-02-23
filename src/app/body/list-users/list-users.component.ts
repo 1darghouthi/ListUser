5cefcb8d1bef6e5e5b38d292aa3aacf42e160730
Matthieu Herrb <anisdarghouthii@gmail.com>
2022-02-23 16:11:19 +0100

    first

--- src/app/body/list-users/list-users.component.ts
+++ src/app/body/list-users/list-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../Models/user';
 import { UserService } from '../Services/user.service';
 
@@ -18,12 +18,14 @@ export class ListUsersComponent implements OnInit {
     
   }
   getuser():void{
-    this.listserv.listUser("https://jsonplaceholder.typicode.com/users").subscribe(users =>{
+    this.listserv.listUser("https://jsonplaceholder.typicode.com/users/").subscribe(users =>{
       this.listhtml=users;
     
       
     });
-
+    
+    
+             
     
   }
 }
